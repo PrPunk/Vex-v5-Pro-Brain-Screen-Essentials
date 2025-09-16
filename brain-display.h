b3cfb81b76c103c714fa9e2b51713e2e0d61b85f
Josiah Schatz <162073626+PrPunk@users.noreply.github.com>
2025-09-16 07:35:31 -0700

    Create brain-display.h

---
+++ brain-display.h
@@ -0,0 +1,34 @@
+/*-------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       brain-display.h                                           */
+/*    Author:       Josiah Schatz                                             */
+/*    Created:      Sat 23 Aug 2025                                           */
+/*    Description:  Framework for brainScreen                                 */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#include <string.h>
+#include <array>
+#include "images.h"
+
+struct Button {
+    unsigned int x;
+    unsigned int y;
+    unsigned int width;
+    unsigned int height;
+    char text[20];
+    unsigned int color;
+    void (*callback)(int);
+    int param;
+};
+
+struct Page {
+    Button *buttons[10];
+    bool hasImage;
+
+    Page() {
+        for (int i = 0; i < 10; i++) {
+            buttons[i] = nullptr;   // initialize
+        }
+    }
+};

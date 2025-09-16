c5a9a88946666de25920829accb8c9c28e671e61
Josiah Schatz <162073626+PrPunk@users.noreply.github.com>
2025-09-16 07:39:50 -0700

    Create images.h

---
+++ images.h
@@ -0,0 +1,11 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       images.h                                                  */
+/*    Author:       Josiah Schatz                                             */
+/*    Created:      Sat 23 Aug 2025                                           */
+/*    Description:  Framework for brainScreen                                 */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+void drawLogo();
